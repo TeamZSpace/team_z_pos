d076843f4d86a03861db01395ca724b9099bbe7c
TeamZSpace <myatbinance@gmail.com>
2026-03-30 14:27:17 +0630

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -13,13 +13,6 @@ export const googleProvider = new GoogleAuthProvider();
 export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
 export const logout = () => signOut(auth);
 
-function isAuthorized() {
-  let authorizedEmails = [
-    "diginestt2026@gmail.com",
-    "myatbinance@gmail.com"
-  ];
-  // ...
-}
 
 // Connection test
 async function testConnection() {
